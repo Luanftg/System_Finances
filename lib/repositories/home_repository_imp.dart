a0344e06aa4a92929580357190e24b39e1c17c95
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-12-01 14:09:09 -0300

    first commit

---
+++ lib/repositories/home_repository_imp.dart
@@ -0,0 +1,21 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:system_finances/models/user_model.dart';
+
+import 'home_repository.dart';
+
+class HomeRepositoryImp implements HomeRepository {
+  @override
+  Future<List<UserModel>> getList() async {
+    try {
+      var response = await Dio().get('http://34.23.32.231:8080/accounts');
+      final users =
+          (response.data as List).map((e) => UserModel.fromJson(e)).toList();
+      return users;
+    } catch (e) {
+      log(e.toString());
+    }
+    return [];
+  }
+}
