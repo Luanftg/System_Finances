1dbd831d2addea123793a5fb4459067b67717e47
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-02 10:39:59 -0300

    [refact] now ValueNotifier listen a homestate

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:system_finances/models/user_model.dart';
 import 'package:system_finances/repositories/home_repository.dart';
+
+import '../state/user_state.dart';
 
 class HomeController {
   final HomeRepository _homeRepository;
   HomeController(this._homeRepository);
 
-  ValueNotifier<List<UserModel>> users = ValueNotifier<List<UserModel>>([]);
+  ValueNotifier<UserState> user = ValueNotifier<UserState>(InitialUserState());
 
   fetch() async {
-    users.value = await _homeRepository.getList();
+    final userLogged = await _homeRepository.getUserLogged();
+    if (userLogged != null) {
+      user.value = SucessUserState(userLogged);
+    } else {
+      user.value = ErrorUserState('Não há usuário Logado!');
+    }
   }
 }
