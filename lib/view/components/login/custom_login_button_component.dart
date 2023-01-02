d6619c78b668b4f62623f48f390d1b8f7ef4f23c
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-02 10:48:54 -0300

    [refact]: add visibility cindition to sign in or sign up

--- lib/view/components/login/custom_login_button_component.dart
+++ lib/view/components/login/custom_login_button_component.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:system_finances/constants/app_colors.dart';
 import 'package:system_finances/state/auth_state.dart';
 import 'package:system_finances/stores/auth_store.dart';
+import 'package:system_finances/view/pages/login_page.dart';
 
+import '../../../constants/app_colors.dart';
 import '../../../models/auth_model.dart';
+import '../../widgets/custom_text_field_widget.dart';
 
-class CustomLoginButtonComponent extends StatelessWidget {
-  //final LoginController loginControler;
+class CustomLoginButtonComponent extends StatefulWidget {
   final AuthStore authStore;
+  static bool isTextButton = true;
 
   const CustomLoginButtonComponent({
     Key? key,
@@ -15,42 +17,61 @@ class CustomLoginButtonComponent extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CustomLoginButtonComponent> createState() =>
+      _CustomLoginButtonComponentState();
+}
+
+class _CustomLoginButtonComponentState
+    extends State<CustomLoginButtonComponent> {
+  @override
   Widget build(BuildContext context) {
     var navContext = Navigator.of(context);
     var scaffoldContext = ScaffoldMessenger.of(context);
     AuthModel? authModel;
     return ValueListenableBuilder<bool>(
-        valueListenable: authStore.inLoader,
-        builder: (_, inLoader, __) => inLoader
-            ? const CircularProgressIndicator()
-            : Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () async {
-                      authModel = AuthModel(authStore.login!, authStore.pass!);
-                      await authStore.authenticate(authModel!);
-                      if (authStore.value is SucessAuthState) {
-                        navContext.pushNamedAndRemoveUntil(
-                            '/home', ((route) => true));
-                      } else if (authStore.value is ErrorAuthState) {
-                        scaffoldContext.showSnackBar(
-                          const SnackBar(
-                            content: Text('Falha ao realizar o login'),
-                            duration: Duration(seconds: 5),
-                          ),
-                        );
-                      }
-                    },
-                    child: const Text('Login'),
+      valueListenable: widget.authStore.inLoader,
+      builder: (_, inLoader, __) => inLoader
+          ? const CircularProgressIndicator()
+          : Column(
+              children: [
+                Visibility(
+                  visible: !CustomLoginButtonComponent.isTextButton,
+                  child: CustomTextFieldWidget(
+                    onChanged: widget.authStore.setName,
+                    label: 'Nome',
                   ),
-                  ElevatedButton(
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    authModel = AuthModel(
+                        widget.authStore.login!, widget.authStore.pass!);
+                    await widget.authStore.authenticate(authModel!);
+                    if (widget.authStore.value is SucessAuthState) {
+                      navContext.pushNamedAndRemoveUntil(
+                          '/home', ((route) => true));
+                    } else if (widget.authStore.value is ErrorAuthState) {
+                      scaffoldContext.showSnackBar(
+                        const SnackBar(
+                          content: Text('Falha ao realizar o login'),
+                          duration: Duration(seconds: 5),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Login'),
+                ),
+                Visibility(
+                  visible: !CustomLoginButtonComponent.isTextButton,
+                  child: ElevatedButton(
                     onPressed: () async {
-                      authModel = AuthModel(authStore.login!, authStore.pass!);
-                      await authStore.register(authModel!);
-                      if (authStore.value is SucessAuthState) {
+                      authModel = AuthModel(
+                          widget.authStore.login!, widget.authStore.pass!,
+                          name: widget.authStore.name);
+                      await widget.authStore.register(authModel!);
+                      if (widget.authStore.value is SucessAuthState) {
                         navContext.pushNamedAndRemoveUntil(
                             '/home', ((route) => true));
-                      } else if (authStore.value is ErrorAuthState) {
+                      } else if (widget.authStore.value is ErrorAuthState) {
                         scaffoldContext.showSnackBar(
                           const SnackBar(
                             content: Text('Falha ao realizar o login'),
@@ -64,8 +85,20 @@ class CustomLoginButtonComponent extends StatelessWidget {
                           MaterialStateProperty.all(AppColors.blue),
                     ),
                     child: const Text('Registrar'),
-                  )
-                ],
-              ));
+                  ),
+                ),
+                Visibility(
+                  visible: CustomLoginButtonComponent.isTextButton,
+                  child: TextButton(
+                      onPressed: () {
+                        LoginPage.isVisible = true;
+                        CustomLoginButtonComponent.isTextButton = false;
+                        setState(() {});
+                      },
+                      child: const Text('Registrar')),
+                ),
+              ],
+            ),
+    );
   }
 }
