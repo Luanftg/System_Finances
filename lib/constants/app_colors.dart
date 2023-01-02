0d643bdc0c0b956bbe23eec108c1430015424aeb
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-02 10:37:54 -0300

    [feat]: add colors

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,8 +4,10 @@ class AppColors {
   AppColors._();
 
   static const Color primary = Colors.green;
+  static Color primaryShade400 = Colors.green.shade400;
   static const Color white = Color(0xFFFFFFFF);
   static const Color white70 = Colors.white70;
+  static const Color white54 = Colors.white54;
   static const Color grey = Color(0xFF79838B);
   static const Color caption = Color(0xFFB8C5D0);
   static const Color black38 = Colors.black38;
