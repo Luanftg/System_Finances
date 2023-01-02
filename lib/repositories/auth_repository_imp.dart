9f58496c7780a4db905f39647d077fffef645e03
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-02 10:43:53 -0300

    [refact]: incude user in FireBaseStore

--- lib/repositories/auth_repository_imp.dart
+++ lib/repositories/auth_repository_imp.dart
@@ -1,12 +1,15 @@
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:system_finances/models/auth_model.dart';
 import 'package:system_finances/repositories/auth_repository.dart';
 
+import '../models/user_model.dart';
+
 class AuthRepositoryImp implements AuthRepository {
   FirebaseAuth get _firebase => FirebaseAuth.instance;
-
+  FirebaseFirestore get _firebaseFirestore => FirebaseFirestore.instance;
   @override
   User? get currentUser => _firebase.currentUser;
 
@@ -15,7 +18,7 @@ class AuthRepositoryImp implements AuthRepository {
     try {
       var signIn = await _firebase.signInWithEmailAndPassword(
           email: authModel.email, password: authModel.password);
-      log(signIn.toString());
+      log('user: ${signIn.user!.toString()}');
       return authModel;
     } catch (e) {
       throw Exception();
@@ -26,11 +29,31 @@ class AuthRepositoryImp implements AuthRepository {
   Future<AuthModel> register(AuthModel authModel) async {
     try {
       await Future.delayed(const Duration(seconds: 2));
-      await _firebase.createUserWithEmailAndPassword(
-          email: authModel.email, password: authModel.password);
+
+      final result = await _firebaseFirestore
+          .collection('users')
+          .where('email', isEqualTo: authModel.email)
+          .get();
+
+      if (result.docs.isEmpty) {
+        final userCredential = await _firebase.createUserWithEmailAndPassword(
+            email: authModel.email, password: authModel.password);
+        final userModel = UserModel.fromFBUser(
+          userCredential.user?.uid ?? '',
+          userCredential.user?.displayName,
+          userCredential.user?.photoURL,
+          userCredential.user?.email,
+        );
+        await userCredential.user?.updateDisplayName(authModel.name);
+        _firebaseFirestore
+            .collection('users')
+            .doc(userCredential.user?.uid)
+            .set(userModel.toMap());
+      }
+
       return authModel;
     } catch (e) {
-      throw Exception();
+      throw Exception(e.toString());
     }
   }
 
